6074c914525be8201badddd96626f525150c8ec8
natejly <natejly@gmail.com>
2024-01-04 17:43:32 -0600

    test

---
+++ makelogin.py
@@ -0,0 +1,23 @@
+import os
+create = False
+user = ''
+password = ''
+key = ''
+'''
+while not create:
+    user = input('Username: ')
+    password = input('Password: ')
+    key = input('MFA key: ')
+    create = input('Create? (y/n): ') == "y"
+    '''
+def addline(str):
+    return 'echo ' + str + ' >> login.txt'
+os.system('touch login.txt')
+os.system(addline(user))
+os.system(addline(password))
+os.system(addline(key))
+print("login file sucessfully created")
+
+
+
+
---
+++ robinstuff.py
@@ -0,0 +1,12 @@
+import robin_stocks.robinhood as robin
+import pyotp
+totp  = pyotp.TOTP("SP5XR4TP34V26ONP").now()
+logindir = '/Users/nately/Documents/login.txt'
+lfile = open(logindir).readlines()
+username = lfile[0][:-1]
+password = lfile[1][:-1]
+login = robin.login(username, password, mfa_code=totp)
+my_stocks = robin.build_holdings()
+for key,value in my_stocks.items():
+   print(key,value)
+#I like rice
